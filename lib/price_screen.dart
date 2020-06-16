25cdc077646e16311ba3c5e11fe5eb582c30e22e
Andrae J Henry <andrae.henry@icloud.com>
2020-06-16 10:36:14 -0400

    Intial commit

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,8 +7,21 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'JMD';
+
+  List <DropdownMenuItem>getDropDownItems(){
+    List<DropdownMenuItem<String>> dropDownItems = [];
+    for (int i = 0; i < currenciesList.length; i++){
+      String currency = currenciesList[i];
+      var newItem = DropdownMenuItem(child: Text(currency), value: currency,);
+      dropDownItems.add(newItem);
+    }
+
+    return dropDownItems;
+  }
   @override
   Widget build(BuildContext context) {
+    getDropDownItems();
     return Scaffold(
       appBar: AppBar(
         title: Text('🤑 Coin Ticker'),
@@ -42,7 +56,14 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              value: selectedCurrency,
+              items: getDropDownItems(),
+              onChanged: (value){
+                setState(() {
+                  selectedCurrency = value;
+                });
+            },),
           ),
         ],
       ),
